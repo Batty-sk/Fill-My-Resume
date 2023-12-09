f9e4f1c7e4225be4860b1e29f1e1382cc55b219d
Batty-sk <saorav.skumar@gmail.com>
2023-12-09 19:30:24 +0530

    utilizing effeicent code

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,105 +1,87 @@
-import React, { useEffect, useState } from 'react'
-import Button from '@mui/material/Button'
-import Chip from '@mui/material/Chip';
-import TextField from '@mui/material/TextField';
-import {Divider} from '@mui/material'
+import React, { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import Chip from "@mui/material/Chip";
+import TextField from "@mui/material/TextField";
+import { Divider } from "@mui/material";
+
+type multivaluesObject = {
+  value: string;
+  chips: string[];
+};
+type multivalues = multivaluesObject;
+type singlevalues = string ;
 
 
-interface userData{
-  [key: string]: string[] | string | null;
+const Form: React.FC = () => {
 
-  name:string|null,
-  firstName:string|null,
-  lastName:string|null,
-  gender:string|null,
-  ethnicity:string|null,
-  email:string[]|string |null,
-  phoneNo:string[]|string| null,
-  jobTitle:string[]|string|null,
-  aboutMe:string[]|string |null,
-  skills:string[]|string |null,
-  languages:string[]|string|null,
-  interPersonalSkills:string[]|string|null,
-  achievements:string[]|string|null,
-  hobbies:string[]|string|null,
-  address:string[]|string|null,
-  workexperience:string[]|string|null,
-  education:string[]|string|null,
-  projects:string[]|string|null,
-  socialMediaLinks:string[]|string|null,
-  whyDoWeHireYou:string[]|string|null,
+  
+  const [name, setName] = useState<singlevalues>(localStorage.getItem('name')|| '');
+  const [firstName, setFirstName] = useState<singlevalues>(localStorage.getItem('firstName')|| '');
+  const [lastName, setLastName] = useState<singlevalues>(localStorage.getItem('lastName')|| '');
+  const [gender, setGender] = useState<singlevalues>(localStorage.getItem('gender')|| '');
+  const [ethnicity, setEthnicity] = useState<singlevalues>(localStorage.getItem('ethnicity')|| '');
+  const [email, setEmail] = useState<singlevalues>(localStorage.getItem('email')|| '');
+  const [phoneNo, setPhoneNo] = useState<singlevalues>(localStorage.getItem('phoneNo')|| '');
+  const [jobTitle, setJobTitle] = useState<singlevalues>(localStorage.getItem('jobTitle')|| '');
+  const [aboutMe, setAboutMe] = useState<singlevalues>(localStorage.getItem('aboutMe')|| '');
+  const signleArray=[setName,setFirstName,setLastName,setGender,setEthnicity,setEmail,setPhoneNo,setJobTitle,setAboutMe]
 
-}
+  const [skills, setSkills] = useState<multivalues>({ value: "", chips: [localStorage.getItem('skills')||'' ] });
+  const [languages, setLanguages] = useState<multivalues>({ value: "", chips: [localStorage.getItem('languages')||'' ] });
+  const [interPersonalSkills, setInterPersonalSkills] = useState<multivalues>({ value: "", chips: [localStorage.getItem('interPersonalSkills')||'' ] });
+  const [achivements,setAchivements]=useState<multivalues>({value:'',chips:[localStorage.getItem('achivements')||'']})
+  const [hobbies, setHobbies] = useState<multivalues>({ value: "", chips: [localStorage.getItem("hobbies") || ""] });
+  const [address, setAddress] = useState<multivalues>({ value: "", chips: [localStorage.getItem("address") || ""] });
+  const [workexperience, setWorkexperience] = useState<multivalues>({ value: "", chips: [localStorage.getItem("workexperience") || ""] });
+  const [education, setEducation] = useState<multivalues>({ value: "", chips: [localStorage.getItem("education") || ""] });
+  const [projects, setProjects] = useState<multivalues>({ value: "", chips: [localStorage.getItem("projects") || ""] });
+  const [socialMediaLinks, setSocialMediaLinks] = useState<multivalues>({ value: "", chips: [localStorage.getItem("socialMediaLinks") || ""] });
+  const [whyDoWeHireYou, setWhyDoWeHireYou] = useState<multivalues>({ value: "", chips: [localStorage.getItem("whyDoWeHireYou") || ""] });
+  const doubleArray=[setSkills,setLanguages,setInterPersonalSkills,setAchivements,setHobbies,setAddress,setWorkexperience,setEducation,setProjects,setSocialMediaLinks,setWhyDoWeHireYou]
 
-const Form:React.FC= () => {
-  const [userData,setUserdata]=useState<userData>({
-    "name":localStorage.getItem('name'),
-    "firstName":localStorage.getItem('firstName'),
-    "lastName":localStorage.getItem('lastName'),
-    "gender":localStorage.getItem('gender'),
-    "ethnicity":localStorage.getItem('ethnicity'),
-    "email":localStorage.getItem('email'),
-    "phoneNo":localStorage.getItem('phoneNo'),
-    "jobTitle":localStorage.getItem('jobTitle'),
-    "aboutMe":localStorage.getItem('aboutMe'),
+  // we need to parse it
 
-    "skills":localStorage.getItem('skills'),
-    "languages":localStorage.getItem('languages'),
-    "interPersonalSkills":localStorage.getItem('interPersonalSkills'),
-    "achievements":localStorage.getItem('achivements'),
-    "hobbies":localStorage.getItem('hobbies'),
-    "address":localStorage.getItem('address'),
-    "workexperience":localStorage.getItem('workexperience'),
-    "education":localStorage.getItem('education'),
-    "projects":localStorage.getItem('projects'),
-    "socialMediaLinks":localStorage.getItem('socialMediaLinks'),
-    "whyDoWeHireYou":localStorage.getItem('whyDoWeHireYou'),
-  })   // we need to parse it
+  let next = 0;
 
-  let next=0
-
-  const handleSubmit=()=>{
-    console.log('subbmitting your data....')
+  const handleSubmit = () => {
+    console.log("subbmitting your data....");
     //localStorage.setItem() // loop through all the items and set it one by one.
-  } 
-/*   <TextField
+  };
+  /*   <TextField
   id="outlined-textarea"
   label="Multiline Placeholder"
   placeholder="Placeholder"
   multiline
 /> */
 
-const [inputValue, setInputValue] = useState <string>('');
-const [chips, setChips] = useState<string[]>([]);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [chips, setChips] = useState<string[]>([]);
 
-const handleInputChange = (event:any) => {
-  setInputValue(event.target.value);
-};
+  const handleInputChange = (event: any) => {
+    setInputValue(event.target.value);
+  };
 
-const handleKeyPress = (event:any) => {
-  if (event.key === 'Enter' && event.target.value.trim() !== '') {
-    setChips([...chips, event.target.value.trim()]);
-    setInputValue('');
-  }
-};
+  const handleKeyPress = (event: any) => {
+    if (event.key === "Enter" && event.target.value.trim() !== "") {
+      setChips([...chips, event.target.value.trim()]);
+      setInputValue("");
+    }
+  };
 
-const handleChipDelete = (chipToDelete:string) => () => {
-  setChips((prevChips) => prevChips.filter((chip) => chip !== chipToDelete));
-};
+  const handleChipDelete = (chipToDelete: string) => () => {
+    setChips((prevChips) => prevChips.filter((chip) => chip !== chipToDelete));
+  };
 
+  const handleOnchange = (value: string, field: string, mutlivalue:boolean=false) => {
 
-const handleOnchange=(value:string,field:string)=>{
-    console.log('running....')
-    setUserdata({...userData,[field]:value} )
-}
+  };
 
-  useEffect(()=>{
-
-  },[])
   return (
-    <div className=''>
-        <div className='flex justify-around flex-wrap' > {/* dyanamically generated would be better option */}
-{/*         {Object.keys(userData).map((key) => {
+    <div className="">
+      <div className="flex justify-around flex-wrap">
+        {" "}
+        {/* dyanamically generated would be better option */}
+        {/*         {Object.keys(userData).map((key) => {
         return  <div className='bg-white' key={key}>
           <p>
             tell me bro
@@ -109,66 +91,50 @@ const handleOnchange=(value:string,field:string)=>{
         }
 
        )} */}
-{Object.keys(userData).map((field,index)=>{
-  if(field === 'skills')
-    next=1
-
-  if(next){
-    <div key={index}>
-<TextField
-      label={field}
-      placeholder="Eg-Programming"
-      value={inputValue}
-      color='success'
-      multiline
-      className='w-[100%]'
-      onChange={handleInputChange}
-      onKeyPress={handleKeyPress}
-    />
-    <div style={{ marginTop: 10 }}>
-      {chips.map((chip, index) => (
-        <Chip
-          key={index}
-          label={chip}
-          onDelete={handleChipDelete(chip)}
-          style={{ marginRight: 5 }}
-        />
-
-      ))}
-    </div>
-    <Divider/>
-
-    </div>
-  }
-  else{
-
-    return<div key={index}>
-        <TextField
+        
+              <TextField
                 label={field}
-                placeholder={`Enter Your ${field}`}
-                value={userData[field]||''}
-                color='warning' 
-                onChange={(event)=>handleOnchange(event.currentTarget.value,field)}
-                >
-                 
+                placeholder={`Enter ${field}`}
+                value={multivalues?.value || ''}
+                color="success"
+                multiline
+                className="w-[100%]"
+                onChange={(x)=>handleOnchange(x.currentTarget.value,field,true)}
+                onKeyPress={handleKeyPress}
+              />
+              <div style={{ marginTop: 10 }}>
+                {chips.map((chip, index) => (
+                  <Chip
+                    key={index}
+                    label={chip}
+                    onDelete={handleChipDelete(chip)}
+                    style={{ marginRight: 5 }}
+                  />
+                ))}
+              </div>
+              <Divider />
+            </div>;
 
-            </TextField>
-        </div>
-  
-  }
-})}
-
-
-
-  </div>
-        <div className=''> 
+              <div key={index}>
+                <TextField
+                  label={field}
+                  placeholder={`Enter Your ${field}`}
+                  value={userData[field]}
+                  color="warning"
+                  onChange={(event) =>
+                    handleOnchange(event.currentTarget.value, field)
+                  }
+                ></TextField>
+              </div>
+    
+      <div className="">
         Yes sir
-          <Button variant="contained" color="inherit" onClick={handleSubmit}>
-              Submit
-          </Button>
-        </div>
-        </div>
-  )
-  }
+        <Button variant="contained" color="inherit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-export default Form
+export default Form;
